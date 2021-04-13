70992847fd23f439120ff8458fd5317d89756fc3
Andy Pham <andy.pham111@gmail.com>
2021-04-13 16:29:41 -0700

    TS tutorial classes

---
+++ throwaway/index.ts
@@ -0,0 +1,8 @@
+// @ts-check
+import { Bear } from "./bear.model";
+import { Animal } from './animal.model';
+
+const bear = new Bear({name: 'Omega', tail: true});
+bear.claws = 3;
+bear.add(1, 2);
+
